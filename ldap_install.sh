e7432d74341a33e190f4868126908e624b42e3dd
anti1346 <anti1346@gmail.com>
2021-09-09 11:32:18 +0900

    스크립트 업데이트

--- ldap_install.sh
+++ ldap_install.sh
@@ -4,6 +4,8 @@
 export $(grep -v '^#' .env | xargs)
 
 ### 호스트 네임 등록
+echo -e "\033[0;31m호스트 네임 등록 START"
+echo -e "\033[0m"
 if [[ -z `grep ldap /etc/hosts` ]];
 then
 cat <<EOF >> /etc/hosts
@@ -11,30 +13,47 @@ $ldapIp1	$ldapHostname1  $ldapFqdn1
 $ldapIp2	$ldapHostname2  $ldapFqdn2
 EOF
 fi
+echo -e "\033[0;31m호스트 네임 등록 END"
+echo -e "\033[0m"
 
 
 ### SELINUX 끄기 ###
+echo -e "\033[0;31mSELINUX 끄기 START"
+echo -e "\033[0m"
 if [ -f /usr/sbin/setenforce ];
 then
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
     /usr/sbin/setenforce 0
 fi
+echo -e "\033[0;31mSELINUX 끄기 END"
+echo -e "\033[0m"
 
 
 ### 방화벽 끄기 ###
+echo -e "\033[0;31m방화벽 끄기 START"
+echo -e "\033[0m"
 if [[ `firewall-cmd --state` = running ]];
 then
     systemctl stop firewalld
     systemctl disable firewalld
 fi
+echo -e "\033[0;31m방화벽 끄기 END"
+echo -e "\033[0m"
 
 
 ### 타임존(TimeZone) 설정 ###
+echo -e "\033[0;31m타임존(TimeZone) 설정 START"
+echo -e "\033[0m"
 timedatectl set-timezone Asia/Seoul
+echo -e "\033[0;31m타임존(TimeZone) 설정 END"
+echo -e "\033[0m"
+
 
 ############################################################################
 ############################################################################
 ############################################################################
+echo -e "\033[0;31mOPENVPN 패키지 설치 및 데몬 기동 START"
+echo -e "\033[0m"
 if [[ -z `rpm -qa | grep openldap-server` ]];
 then
     yum reinstall -y --quiet compat-openldap \
@@ -56,7 +75,8 @@ fi
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 chown -R ldap:ldap /var/lib/ldap/DB_CONFIG
 systemctl restart slapd
-
+echo -e "\033[0;31mOPENVPN 패키지 설치 및 데몬 기동 END"
+echo -e "\033[0m"
 ############################################################################
 ############################################################################
 ############################################################################
@@ -67,6 +87,8 @@ if [ ! -d $ldifDirectory ]; then
 fi
 
 
+echo -e "\033[0;31mchrootpw.ldif START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/chrootpw.ldif
 dn: olcDatabase={0}config,cn=config
 changetype: modify
@@ -74,10 +96,13 @@ add: olcRootPW
 #replace: olcRootPW
 olcRootPW: ${OLolcRootPW}
 EOF
-
 ldapadd -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/chrootpw.ldif
+echo -e "\033[0;31mchrootpw.ldif END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31m기본 스키마 적용 START"
+echo -e "\033[0m"
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/core.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
@@ -87,8 +112,12 @@ ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/ppolicy.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/collective.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/misc.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/duaconf.ldif
+echo -e "\033[0;31m기본 스키마 적용 END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31mdb.ldif 생성 START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/db.ldif
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
@@ -105,14 +134,16 @@ changetype: modify
 replace: olcRootPW
 olcRootPW: ${OLolcRootPW}
 EOF
-
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/db.ldif
+echo -e "\033[0;31mdb.ldif 생성 END"
+echo -e "\033[0m"
 
 
 ###certs
+echo -e "\033[0;31mcerts 생성 START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/certificate.sh
 #!/bin/bash
-
 openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes \
 -out /etc/openldap/certs/${OLDomain}.crt \
 -keyout /etc/openldap/certs/${OLDomain}.key \
@@ -120,7 +151,6 @@ openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes \
 EOF
 chmod +x ${ldifDirectory}/certificate.sh
 sh ${ldifDirectory}/certificate.sh
-
 chown -R ldap:ldap /etc/openldap/certs/${OLDomain}.*
 chmod o-r /etc/openldap/certs/${OLDomain}.key
 
@@ -135,7 +165,6 @@ changetype: modify
 replace: olcTLSCertificateFile
 olcTLSCertificateFile: /etc/openldap/certs/${OLDomain}.crt
 EOF
-
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/certs.ldif
 
 cat <<EOF > ${ldifDirectory}/certs.ldif
@@ -149,13 +178,15 @@ changetype: modify
 replace: olcTLSCertificateKeyFile
 olcTLSCertificateKeyFile: /etc/openldap/certs/${OLDomain}.key
 EOF
-
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/certs.ldif
-
 sed -i 's/SLAPD_URLS=\"ldapi:\/\/\/\ ldap:\/\/\/\"/SLAPD_URLS=\"ldapi:\/\/\/\ ldap:\/\/\/ ldaps:\/\/\/\"/' /etc/sysconfig/slapd
+echo -e "\033[0;31mcerts 생성 END"
+echo -e "\033[0m"
 
 
 ###rsyslog(slapd)
+echo -e "\033[0;31mRSYSLOG START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/rsyslog.ldif
 dn: cn=config
 changetype: modify
@@ -169,11 +200,13 @@ then
     echo -e "local4.*\t\t\t\t\t\t/var/log/slapd.log" >> /etc/rsyslog.conf
     systemctl restart rsyslog
 fi
-
-
 systemctl restart slapd
+echo -e "\033[0;31mRSYSLOG END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31mmonitor.ldif START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/monitor.ldif
 dn: olcDatabase={1}monitor,cn=config
 changetype: modify
@@ -181,10 +214,13 @@ replace: olcAccess
 olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" read
   by dn.base="cn=Manager,dc=ldap,dc=mt,dc=co,dc=kr" read by * none
 EOF
-
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/monitor.ldif
+echo -e "\033[0;31mmonitor.ldif END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31macl.ldif START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/acl.ldif
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
@@ -194,13 +230,16 @@ olcAccess: {0}to attrs=userPassword,shadowLastChange by
 olcAccess: {1}to dn.base="" by * read
 olcAccess: {2}to * by dn="cn=Manager,${OLolcSuffix}" write by * read
 EOF
-
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/acl.ldif
+echo -e "\033[0;31macl.ldif END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31mbaseTop.ldif START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/baseTop.ldif
 dn: ${OLolcSuffix}
-dc: ldap
+dc: tldap
 objectClass: top
 objectClass: dcObject
 objectclass: organization
@@ -212,8 +251,12 @@ objectClass: organizationalRole
 description: Directory Manager
 EOF
 ldapadd -x -D cn=Manager,${OLolcSuffix} -w ${OLolcRootPPW} -f ${ldifDirectory}/baseTop.ldif
+echo -e "\033[0;31mbaseTop.ldif END"
+echo -e "\033[0m"
 
 
+echo -e "\033[0;31mbaseOu.ldif START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/baseOu.ldif
 dn: ou=People,${OLolcSuffix}
 ou: People
@@ -223,11 +266,14 @@ dn: ou=Groups,${OLolcSuffix}
 ou: Groups
 objectClass: organizationalUnit
 EOF
-
 ldapadd -x -D cn=Manager,${OLolcSuffix} -w ${OLolcRootPPW} -f ${ldifDirectory}/baseOu.ldif
+echo -e "\033[0;31mbaseOu.ldif END"
+echo -e "\033[0m"
 
 
-###ppolicy
+###PPOLICY
+echo -e "\033[0;31mPPOLICY START"
+echo -e "\033[0m"
 cat <<EOF > ${ldifDirectory}/ppolicy-module.ldif
 dn: cn=module{0},cn=config
 cn: module
@@ -235,7 +281,6 @@ objectClass: olcModuleList
 olcModuleLoad: ppolicy.la
 olcModulePath: /usr/lib64/openldap
 EOF
-
 ldapadd -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/ppolicy-module.ldif
 
 cat <<EOF > ${ldifDirectory}/ppolicy-ou.ldif
@@ -245,7 +290,6 @@ objectClass: top
 objectClass: extensibleObject
 objectClass: organizationalUnit
 EOF
-
 ldapmodify -a -x -D cn=Manager,${OLolcSuffix} -w ${OLolcRootPPW} -f ${ldifDirectory}/ppolicy-ou.ldif
 
 cat <<EOF > ${ldifDirectory}/ppolicy-overlay.ldif
@@ -257,7 +301,6 @@ olcPPolicyDefault: cn=default,ou=Policies,${OLolcSuffix}
 olcPPolicyUseLockout: FALSE
 olcPPolicyHashCleartext: TRUE
 EOF
-
 ldapadd -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/ppolicy-overlay.ldif
 
 cat <<EOF > ${ldifDirectory}/ppolicy-password.ldif
@@ -283,10 +326,14 @@ pwdGraceAuthNLimit: 0
 pwdMustChange: FALSE
 pwdSafeModify: FALSE
 EOF
-
 ldapadd -x -D cn=Manager,${OLolcSuffix} -w ${OLolcRootPPW} -f ${ldifDirectory}/ppolicy-password.ldif
+echo -e "\033[0;31mPPOLICY END"
+echo -e "\033[0m"
+
 
 ###syncprov
+echo -e "\033[0;31msyncprov START"
+echo -e "\033[0m"
 if [ "$1" == "slave" ];
 then
 cat <<EOF > ${ldifDirectory}/syncrepl-slave-replication.ldif
@@ -316,7 +363,6 @@ objectClass: simpleSecurityObject
 description: Replication User
 userPassword: qwER12#$
 EOF
-
     ldapadd -x -D cn=Manager,${OLolcSuffix} -w ${OLolcRootPPW} -f ${ldifDirectory}/syncrepl-master-replicator.ldif
 
 cat <<EOF > ${ldifDirectory}/syncrepl-master-module.ldif
@@ -326,7 +372,6 @@ cn: module
 olcModulePath: /usr/lib64/openldap
 olcModuleLoad: syncprov.la
 EOF
-
     ldapadd -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/syncrepl-master-module.ldif
 
 cat <<EOF > ${ldifDirectory}/syncrepl-master-syncprov.ldif
@@ -336,10 +381,14 @@ objectClass: olcSyncProvConfig
 olcOverlay: syncprov
 olcSpSessionLog: 100
 EOF
-
     ldapadd -Y EXTERNAL -H ldapi:/// -f ${ldifDirectory}/syncrepl-master-syncprov.ldif
     echo -e "syncprov master\n"
 fi
+echo -e "\033[0;31msyncprov END"
+echo -e "\033[0m"
 
+echo -e "\033[0;36m"
 ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b cn=schema,cn=config dn
+echo -e "\033[0;32m"
 ldapsearch -H ldapi:/// -Y EXTERNAL -b "cn=config" -LLL -Q "olcDatabase=*" dn
+echo -e "\033[0m"
